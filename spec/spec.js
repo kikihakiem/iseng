0d911da084006b12e475a66e9bfc05d78a304946
Kiki <kiki.hakiem@gmail.com>
2016-05-28 02:22:50 +0700

    Implement .toPattern function

--- spec/spec.js
+++ spec/spec.js
@@ -40,5 +40,26 @@ describe("Iseng", function() {
       expect(iseng.toPattern).toHaveBeenCalledWith(testWord);
     });
   });
+
+  describe("toPattern", function() {
+    it("translates vowels and consonants to 0 and 1 respectively", function() {
+      expect(iseng.toPattern("Foo")).toEqual("100");
+      expect(iseng.toPattern("bar")).toEqual("101");
+      expect(iseng.toPattern("ICU")).toEqual("010");
+    });
+
+    it("cleans up whitespaces", function() {
+      expect(iseng.toPattern(" word ")).toEqual("1011");
+    });
+
+    it("leaves 'y' char untranslated", function() {
+      expect(iseng.toPattern("cry")).toEqual("11y");
+    });
+
+    it("leaves possesive suffix untranslated", function() {
+      expect(iseng.toPattern("dog's")).toEqual("101's");
+      expect(iseng.toPattern("ra'salghul")).toEqual("10'1011101");
+    })
+  });
 });
 
